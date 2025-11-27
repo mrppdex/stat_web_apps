13456558d509c464e08c087e3eb630e53b4100b7
Pawel Piela <mrppdex@Pawels-MacBook-Pro.local>
2025-11-27 11:05:46 +0000

    fix: update vite base path to absolute url for github pages

--- cdisc-drawing-board-react/vite.config.js
+++ cdisc-drawing-board-react/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/stat_web_apps/cdisc-drawing-board-react/dist/',
 })
